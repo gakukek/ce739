f300b517df0c02bcbf76d2d127e3dd8ec4a32349
gakukek <darrylclaylayaya@gmail.com>
2025-12-20 19:40:39 +0700

    fix cors

--- main.py
+++ main.py
@@ -51,8 +51,8 @@ _origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://aquascape.onrender.com",
-    "https://ce739-fe.darrylclaylayaya.workers.dev/",
-    "https://ce739-fe.pages.dev/",
+    "https://ce739-fe.darrylclaylayaya.workers.dev",
+    "https://ce739-fe.pages.dev",
 ]
 
 app.add_middleware(
